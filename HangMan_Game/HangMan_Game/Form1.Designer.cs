624dd316654474b4df6fa306644df8ac31a6f2cf
Mehmet Demir <127750107+MehmetDemr@users.noreply.github.com>
2024-05-30 15:48:47 +0300

    Add files via upload

---
+++ HangMan_Game/HangMan_Game/Form1.Designer.cs
@@ -0,0 +1,360 @@
+﻿namespace HangMan_Game
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            tabPage2 = new TabPage();
+            button8 = new Button();
+            label6 = new Label();
+            label5 = new Label();
+            pictureBox2 = new PictureBox();
+            pictureBox1 = new PictureBox();
+            label4 = new Label();
+            button6 = new Button();
+            richTextBox1 = new RichTextBox();
+            textBox3 = new TextBox();
+            textBox2 = new TextBox();
+            label3 = new Label();
+            button5 = new Button();
+            label2 = new Label();
+            tabPage1 = new TabPage();
+            button7 = new Button();
+            button4 = new Button();
+            button3 = new Button();
+            button2 = new Button();
+            button1 = new Button();
+            label1 = new Label();
+            tabControl1 = new TabControl();
+            tabPage2.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)pictureBox2).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)pictureBox1).BeginInit();
+            tabPage1.SuspendLayout();
+            tabControl1.SuspendLayout();
+            SuspendLayout();
+            // 
+            // tabPage2
+            // 
+            tabPage2.BackColor = Color.FromArgb(255, 192, 128);
+            tabPage2.Controls.Add(button8);
+            tabPage2.Controls.Add(label6);
+            tabPage2.Controls.Add(label5);
+            tabPage2.Controls.Add(pictureBox2);
+            tabPage2.Controls.Add(pictureBox1);
+            tabPage2.Controls.Add(label4);
+            tabPage2.Controls.Add(button6);
+            tabPage2.Controls.Add(richTextBox1);
+            tabPage2.Controls.Add(textBox3);
+            tabPage2.Controls.Add(textBox2);
+            tabPage2.Controls.Add(label3);
+            tabPage2.Controls.Add(button5);
+            tabPage2.Controls.Add(label2);
+            tabPage2.Font = new Font("Segoe UI", 19F, FontStyle.Regular, GraphicsUnit.Point);
+            tabPage2.ForeColor = SystemColors.ControlText;
+            tabPage2.ImeMode = ImeMode.NoControl;
+            tabPage2.Location = new Point(4, 29);
+            tabPage2.Name = "tabPage2";
+            tabPage2.Padding = new Padding(3);
+            tabPage2.Size = new Size(1792, 1238);
+            tabPage2.TabIndex = 1;
+            tabPage2.Text = "Game";
+            tabPage2.Click += tabPage2_Click;
+            // 
+            // button8
+            // 
+            button8.BackColor = Color.Cyan;
+            button8.Location = new Point(8, 641);
+            button8.Name = "button8";
+            button8.Size = new Size(332, 159);
+            button8.TabIndex = 14;
+            button8.Text = "<-Main Menu";
+            button8.UseVisualStyleBackColor = false;
+            button8.Click += button8_Click;
+            // 
+            // label6
+            // 
+            label6.AutoSize = true;
+            label6.Font = new Font("Segoe UI", 29F, FontStyle.Regular, GraphicsUnit.Point);
+            label6.Location = new Point(3, 9);
+            label6.Name = "label6";
+            label6.Size = new Size(0, 66);
+            label6.TabIndex = 13;
+            label6.Visible = false;
+            // 
+            // label5
+            // 
+            label5.AutoSize = true;
+            label5.Font = new Font("Segoe UI", 20F, FontStyle.Regular, GraphicsUnit.Point);
+            label5.Location = new Point(1044, 62);
+            label5.Name = "label5";
+            label5.Size = new Size(146, 46);
+            label5.TabIndex = 12;
+            label5.Text = "HEALTH:";
+            // 
+            // pictureBox2
+            // 
+            pictureBox2.Location = new Point(1144, 366);
+            pictureBox2.Name = "pictureBox2";
+            pictureBox2.Size = new Size(515, 398);
+            pictureBox2.TabIndex = 11;
+            pictureBox2.TabStop = false;
+            pictureBox2.Click += pictureBox2_Click;
+            // 
+            // pictureBox1
+            // 
+            pictureBox1.Location = new Point(1484, 0);
+            pictureBox1.Name = "pictureBox1";
+            pictureBox1.Size = new Size(282, 186);
+            pictureBox1.SizeMode = PictureBoxSizeMode.StretchImage;
+            pictureBox1.TabIndex = 10;
+            pictureBox1.TabStop = false;
+            pictureBox1.Click += pictureBox1_Click_2;
+            // 
+            // label4
+            // 
+            label4.AutoSize = true;
+            label4.Font = new Font("Segoe UI", 19F, FontStyle.Regular, GraphicsUnit.Point);
+            label4.Location = new Point(457, 112);
+            label4.Name = "label4";
+            label4.Size = new Size(0, 45);
+            label4.TabIndex = 9;
+            label4.Click += label4_Click;
+            // 
+            // button6
+            // 
+            button6.BackColor = Color.FromArgb(128, 255, 128);
+            button6.Location = new Point(488, 662);
+            button6.Name = "button6";
+            button6.Size = new Size(332, 138);
+            button6.TabIndex = 8;
+            button6.Text = "START!!";
+            button6.UseVisualStyleBackColor = false;
+            button6.Click += button6_Click;
+            // 
+            // richTextBox1
+            // 
+            richTextBox1.Font = new Font("Segoe UI", 25F, FontStyle.Regular, GraphicsUnit.Point);
+            richTextBox1.ForeColor = SystemColors.ActiveCaptionText;
+            richTextBox1.Location = new Point(457, 218);
+            richTextBox1.Name = "richTextBox1";
+            richTextBox1.Size = new Size(419, 144);
+            richTextBox1.TabIndex = 7;
+            richTextBox1.Text = "";
+            richTextBox1.TextChanged += richTextBox1_TextChanged;
+            // 
+            // textBox3
+            // 
+            textBox3.BackColor = SystemColors.Info;
+            textBox3.Font = new Font("Segoe UI", 25F, FontStyle.Regular, GraphicsUnit.Point);
+            textBox3.Location = new Point(1196, 0);
+            textBox3.Multiline = true;
+            textBox3.Name = "textBox3";
+            textBox3.Size = new Size(282, 186);
+            textBox3.TabIndex = 6;
+            textBox3.TextChanged += textBox3_TextChanged;
+            // 
+            // textBox2
+            // 
+            textBox2.BackColor = Color.LightSeaGreen;
+            textBox2.Font = new Font("Segoe UI", 25F, FontStyle.Regular, GraphicsUnit.Point);
+            textBox2.Location = new Point(457, 368);
+            textBox2.Multiline = true;
+            textBox2.Name = "textBox2";
+            textBox2.Size = new Size(419, 144);
+            textBox2.TabIndex = 3;
+            textBox2.TextChanged += textBox2_TextChanged;
+            // 
+            // label3
+            // 
+            label3.AutoSize = true;
+            label3.Font = new Font("Segoe UI", 26F, FontStyle.Regular, GraphicsUnit.Point);
+            label3.Location = new Point(806, 48);
+            label3.Name = "label3";
+            label3.Size = new Size(0, 60);
+            label3.TabIndex = 5;
+            // 
+            // button5
+            // 
+            button5.BackColor = Color.Silver;
+            button5.Location = new Point(488, 518);
+            button5.Name = "button5";
+            button5.Size = new Size(332, 138);
+            button5.TabIndex = 2;
+            button5.Text = "GUESS!!";
+            button5.UseVisualStyleBackColor = false;
+            button5.Click += button5_Click;
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.Font = new Font("Segoe UI", 18F, FontStyle.Regular, GraphicsUnit.Point);
+            label2.Location = new Point(6, 13);
+            label2.Name = "label2";
+            label2.Size = new Size(0, 41);
+            label2.TabIndex = 0;
+            // 
+            // tabPage1
+            // 
+            tabPage1.BackColor = Color.FromArgb(255, 192, 128);
+            tabPage1.Controls.Add(button7);
+            tabPage1.Controls.Add(button4);
+            tabPage1.Controls.Add(button3);
+            tabPage1.Controls.Add(button2);
+            tabPage1.Controls.Add(button1);
+            tabPage1.Controls.Add(label1);
+            tabPage1.ForeColor = SystemColors.ActiveCaptionText;
+            tabPage1.Location = new Point(4, 29);
+            tabPage1.Name = "tabPage1";
+            tabPage1.Padding = new Padding(3);
+            tabPage1.Size = new Size(1792, 1238);
+            tabPage1.TabIndex = 0;
+            tabPage1.Text = "Main Menu";
+            tabPage1.Click += tabPage1_Click;
+            // 
+            // button7
+            // 
+            button7.BackColor = Color.Cyan;
+            button7.Location = new Point(542, 122);
+            button7.Name = "button7";
+            button7.Size = new Size(613, 58);
+            button7.TabIndex = 5;
+            button7.Text = "LETS PLAY->";
+            button7.UseVisualStyleBackColor = false;
+            button7.Visible = false;
+            button7.Click += button7_Click_1;
+            // 
+            // button4
+            // 
+            button4.BackColor = Color.FromArgb(0, 192, 192);
+            button4.Location = new Point(542, 657);
+            button4.Name = "button4";
+            button4.Size = new Size(613, 143);
+            button4.TabIndex = 4;
+            button4.Text = "Exit";
+            button4.UseVisualStyleBackColor = false;
+            button4.Click += button4_Click;
+            // 
+            // button3
+            // 
+            button3.BackColor = Color.FromArgb(192, 0, 0);
+            button3.Location = new Point(542, 508);
+            button3.Name = "button3";
+            button3.Size = new Size(613, 143);
+            button3.TabIndex = 3;
+            button3.Text = "Hard Mode";
+            button3.UseVisualStyleBackColor = false;
+            button3.Click += button3_Click;
+            // 
+            // button2
+            // 
+            button2.BackColor = Color.Gold;
+            button2.Location = new Point(542, 359);
+            button2.Name = "button2";
+            button2.Size = new Size(613, 143);
+            button2.TabIndex = 2;
+            button2.Text = "Medium Mode";
+            button2.UseVisualStyleBackColor = false;
+            button2.Click += button2_Click;
+            // 
+            // button1
+            // 
+            button1.BackColor = Color.FromArgb(128, 255, 128);
+            button1.Location = new Point(542, 210);
+            button1.Name = "button1";
+            button1.Size = new Size(613, 143);
+            button1.TabIndex = 1;
+            button1.Text = "Easy Mode";
+            button1.UseVisualStyleBackColor = false;
+            button1.Click += button1_Click;
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Font = new Font("Segoe UI", 26.2F, FontStyle.Regular, GraphicsUnit.Point);
+            label1.Location = new Point(542, 31);
+            label1.Name = "label1";
+            label1.Size = new Size(602, 60);
+            label1.TabIndex = 0;
+            label1.Text = "Welcome To Hang Man Game";
+            label1.Click += label1_Click;
+            // 
+            // tabControl1
+            // 
+            tabControl1.Controls.Add(tabPage1);
+            tabControl1.Controls.Add(tabPage2);
+            tabControl1.Location = new Point(0, 12);
+            tabControl1.Name = "tabControl1";
+            tabControl1.SelectedIndex = 0;
+            tabControl1.Size = new Size(1800, 1271);
+            tabControl1.TabIndex = 0;
+            tabControl1.TabStop = false;
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(8F, 20F);
+            AutoScaleMode = AutoScaleMode.Font;
+            BackColor = Color.FromArgb(255, 192, 128);
+            ClientSize = new Size(1782, 853);
+            Controls.Add(tabControl1);
+            Name = "Form1";
+            Text = "Form1";
+            tabPage2.ResumeLayout(false);
+            tabPage2.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)pictureBox2).EndInit();
+            ((System.ComponentModel.ISupportInitialize)pictureBox1).EndInit();
+            tabPage1.ResumeLayout(false);
+            tabPage1.PerformLayout();
+            tabControl1.ResumeLayout(false);
+            ResumeLayout(false);
+        }
+
+        #endregion
+
+        private TabPage tabPage2;
+        private PictureBox pictureBox2;
+        private PictureBox pictureBox1;
+        private Label label4;
+        private Button button6;
+        private RichTextBox richTextBox1;
+        private TextBox textBox3;
+        private TextBox textBox2;
+        private Label label3;
+        private Button button5;
+        private Label label2;
+        private TabPage tabPage1;
+        private Button button4;
+        private Button button3;
+        private Button button2;
+        private Button button1;
+        private Label label1;
+        private TabControl tabControl1;
+        private Label label5;
+        private Button button7;
+        private Label label6;
+        private Button button8;
+    }
+}
